1b53afebc502f5804f12501131e4794f5d1c51a0
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-10-07 23:57:58 +0530

    added controller for register

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"zeroHunger/database"
+	"zeroHunger/routes"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -11,5 +12,6 @@ func main() {
 	database.Connection()
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{AllowCredentials: true}))
+	routes.Setup(app)
 	app.Listen(":3000")
 }
